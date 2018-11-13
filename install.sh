7d2fb72e3d09eb4befdedaab948f2bc96e6c634b
Nicolas Jaremek <nick13jaremek@gmail.com>
2018-11-13 12:21:27 +0100

    Added install script for vim files.

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+log() {
+    echo -e "\033[1m\033[32mINFO:\033[0m $1"
+}
+
+if [ -d "$HOME/.vim" ]
+then
+    ts=$(date +"%d%m%Y@%H:%M:%S")
+    log "Making backup of '\$HOME/.vim' to '\$HOME/.vim.$ts'"
+    mv $HOME/{.vim, .vim.$ts}
+fi
+
+log "Cloning repository..."
+git clone https://github.com/nick13jaremek/dotvim.git $HOME/.vim
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
+cd $HOME/.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
